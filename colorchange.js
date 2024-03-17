fa8e829a5c85dbc386d1d59b5402f08c72db3584
pragyanparimita10 <sahoopragyanparamita152@gmail.com>
2024-03-17 11:50:23 +0530

    add colorchanger

---
+++ colorchange.js
@@ -0,0 +1,23 @@
+const buttons=document.querySelectorAll("button");
+// console.log(buttons);
+const body=document.body;
+buttons.forEach((button)=>{
+    button.addEventListener("click",(e)=>{
+        console.log(e.target.id);
+        if(e.target.id==='grey'){
+            body.style.backgroundColor='grey';
+        }
+        if(e.target.id==='white'){
+            body.style.backgroundColor=e.target.id;
+        }
+        if(e.target.id==='blue'){
+            body.style.backgroundColor=e.target.id;
+        }
+        if(e.target.id==='yellow'){
+            body.style.backgroundColor=e.target.id;
+        }
+        if(e.target.id==='purple'){
+            body.style.backgroundColor=e.target.id;
+        }
+    })
+})
